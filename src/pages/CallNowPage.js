1630dde1b3e8ab1b88b84aff5c886a417f72e23a
Theo LetsGetDigi <info@letsgetdigi.com>
2023-03-23 00:40:46 +0200

    First Commit

---
+++ src/pages/CallNowPage.js
@@ -0,0 +1,38 @@
+import { Helmet } from 'react-helmet-async';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Grid, Container, Typography, Card, Input } from '@mui/material';
+// components
+import Iconify from '../components/iconify';
+
+// ----------------------------------------------------------------------
+
+export default function CalNowPage() {
+    const theme = useTheme();
+
+    return (
+        <>
+            <Helmet>
+                <title> Call Now | DigiLeads </title>
+            </Helmet>
+
+            <Container maxWidth="xl">
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                    Call Now
+                </Typography>
+                <Container>
+                    <Grid item xs={12} md={6} lg={8}>
+                        <Card>
+                            <form>
+                                <h2>Phonenumber</h2>
+                                <Input type="text" />
+                                <h2>Website Url</h2>
+                                <input type="text" />
+                            </form>
+                        </Card>
+                    </Grid>
+                </Container>
+            </Container>
+        </>
+    );
+}
